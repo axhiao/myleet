02979f51476aae702c77358b0e308c42ffd6b7a0
axhiao <axhiao@gmail.com>
2015-09-02 17:06:34 +0800

    memcpy.cpp

--- 1A/README.md
+++ 1A/README.md
@@ -1,25 +1,25 @@
 
 
 
-1. 二叉树的遍历（递归与迭代，共６种方式）, [click me. :smile:](./TraverseTree)
+1. [二叉树的遍历（递归与迭代，共６种方式） :smile:](./TraverseTree)
 
-2. 第k大的数，以及中位数（基于快排思想实现）.[click me](./kth-largest_and_media)
+2. [第k大的数，以及中位数（基于快排思想实现）](./kth-largest_and_media)
 
-3. 内存拷贝函数, memory copy. [click me](./memcpy)
+3. [内存拷贝函数, memory copy.](./memcpy)
 
-4. 数组中未出现的最小正整数。　[click me](./missNumber)
+4. [数组中未出现的最小正整数.](./missNumber)
 
-5. 随机洗牌算法. [click me](./shuffle)
+5. [随机洗牌算法.](./shuffle)
 
-6. 计算数组的小和（基于归并思想）　[click me](./smallSum)
+6. [计算数组的小和（基于归并思想）](./smallSum)
 
-7. 各种内排序（计数，堆，归并，快排，希尔等）. [click me](./sort)
+7. [各种内排序（计数，堆，归并，快排，希尔等）.](./sort)
 
-8. 逆序对(基于归并思想). [click me](./sort/Inversions.cpp)
+8. [逆序对(基于归并思想).](./sort/Inversions.cpp)
 
-9. 剑指offer中的各种算法习题. [click me](./sword_to_offer)
+9. [剑指offer中的各种算法习题.](./sword_to_offer)
 
-10. 排序之后相邻两数的最大差值. [click me](./maxGap)
+10. [排序之后相邻两数的最大差值.](./maxGap)
 
 11. [逆序打印整数，要求递归实现](./strings/rv.cpp).
 
--- 1A/memcpy/memcpy.cpp
+++ 1A/memcpy/memcpy.cpp
@@ -44,5 +44,46 @@ void mymemcpy(void *dst, const void *src, size_t num)
 }
 
 // optimize this code 
+void mymemcpy_opt(void *dst, const void *src, size_t num)
+{
+	if (dst == NULL || src == NULL || num <= 0)
+		return;
 
-//可以每次复制int也就是4个字节，而不是char，1个字节
+	int wordnum = num / 4;
+	int slice = num % 4;
+
+	const int* pintsrc = (const int*)src;
+	int* pintdst = (int*)dst;
+
+	// reverse
+	if( (char*)dst > (char*)src && (char*)dst < (char*)src + num )
+	{
+		// copy 4 bytes reverse
+		for (size_t i = wordnum - 1; i != -1; --i)
+		{
+			pintdst[i] = pintsrc[i];
+		}
+		// copy one byte reverse
+		const char *pcharsrc = (const char*) pintsrc;
+		char *pchardst = (char*)pintdst;
+		for (size_t i = slice - 1; i != -1; --i)
+		{
+			pchardst[i] =  pcharsrc[i];
+		}
+	}
+	else
+	{
+		// copy 4 bytes
+		while(wordnum--)
+		{
+			*pintdst++ = *pintsrc++;
+		}
+		// copy one byte
+		const char *pcharsrc = (const char*) pintsrc;
+		char *pchardst = (char*)pintdst;
+		while(slice--)
+		{
+			*pchardst++ = *pcharsrc++;
+		}
+	}
+}
